8c2c19ca8538b173a3ee1e8bb47ab007a31677fa
Ben Fedidat <ben@fedidat.com>
2024-12-10 00:33:13 +0200

    Improved day 9

--- day_9/main.cc
+++ day_9/main.cc
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <map>
 #include <set>
 
 void part1();
@@ -22,58 +21,59 @@ std::vector<int> parseFile(std::string fileName)
     std::getline(file, line);
     file.close();
     std::vector<int> digits = std::vector<int>();
-    for (char c : line) {
+    for (char c : line)
         digits.push_back(c - '0');
-    }
     return digits;
 }
 
-void part1() {
-    std::vector<int> input = parseFile("input.txt");
-
-    // Put together the intiial file map
-    std::vector<uint16_t> fileMap = std::vector<uint16_t>();
-    uint16_t fileId = 0;
-    for (int i=0; i<input.size(); i+=2) {
-        for (int j=0; j<input[i]; j++)
-            fileMap.push_back(fileId);
-        fileId++;
-        if (i+1 < input.size())
-            for (int j=0; j<input[i+1]; j++)
-                fileMap.push_back(UINT16_MAX);
-    }
-
-    // Defragment
-    for (int startIndex = 0, endIndex = fileMap.size()-1; ;) {
-        while (fileMap[startIndex] != UINT16_MAX && startIndex < endIndex)
-            startIndex++;
-        while (fileMap[endIndex] == UINT16_MAX)
-            endIndex--;
-        if (startIndex >= endIndex)
-            break;
-        fileMap[startIndex] = fileMap[endIndex];
-        fileMap[endIndex] = UINT16_MAX;
-    }
-
-    // Compute checksum
-    long sum = 0;
-    for (int i=0; i<fileMap.size() && fileMap[i] != UINT16_MAX; i++)
-        sum += i * fileMap[i];
-    std::cout << sum << std::endl;
-}
-
-void part2() {
-    std::vector<int> input = parseFile("input.txt");
-
-    // Put together the intiial file map
+std::vector<std::pair<int, int>> getFilemap(std::vector<int> input) {
     std::vector<std::pair<int, int>> filemap = std::vector<std::pair<int, int>>();
     for (int i=0; i<input.size(); i+=2) {
         filemap.push_back({i/2, input[i]});
         if (i+1 < input.size())
             filemap.push_back({UINT16_MAX, input[i+1]});
     }
+    return filemap;
+}
 
-    // Defragment
+long getChecksum(std::vector<std::pair<int, int>> filemap) {
+    long sum = 0, pos = 0;
+    for (int i=0; i<filemap.size(); i++) {
+        if (filemap[i].first != UINT16_MAX)
+            for (int j=pos; j<pos+filemap[i].second; j++)
+                sum += filemap[i].first * j;
+        pos += filemap[i].second;
+    }
+    return sum;
+}
+
+void part1() {
+    std::vector<int> input = parseFile("input.txt");
+    std::vector<std::pair<int, int>> filemap = getFilemap(input);
+    for(int curBlock = filemap.size()-1, curFree = 1; curFree < curBlock; curBlock++, curFree++) {
+        if (filemap[curBlock].second <= 0) {
+            filemap.erase(filemap.begin() + curBlock);
+            curBlock--;
+        }
+        while(filemap[curBlock].first == UINT16_MAX)
+            curBlock--;
+        if (filemap[curFree].second <= 0) {
+            filemap.erase(filemap.begin() + curFree);
+            curBlock--;
+        }
+        while(filemap[curFree].first != UINT16_MAX)
+            curFree++;
+        int toCopy = std::min(filemap[curFree].second, filemap[curBlock].second);
+        filemap[curFree].second -= toCopy;
+        filemap[curBlock].second -= toCopy;
+        filemap.insert(filemap.begin() + curFree, {filemap[curBlock].first, toCopy});
+    }
+    std::cout << "Part 1: " << getChecksum(filemap) << std::endl;
+}
+
+void part2() {
+    std::vector<int> input = parseFile("input.txt");
+    std::vector<std::pair<int, int>> filemap = getFilemap(input);
     std::set<int> visited = std::set<int>();
     for (int i=filemap.size() - (filemap.size() % 2 == 0 ? 2 : 1); i>=0; i--) {
         if(filemap[i].first == UINT16_MAX || visited.contains(filemap[i].first))
@@ -89,14 +89,5 @@ void part2() {
             }
         }
     }
-
-    // Compute checksum
-    long sum = 0, pos = 0;
-    for (int i=0; i<filemap.size(); i++) {
-        if (filemap[i].first != UINT16_MAX)
-            for (int j=pos; j<pos+filemap[i].second; j++)
-                sum += filemap[i].first * j;
-        pos += filemap[i].second;
-    }
-    std::cout << sum << std::endl;
+    std::cout << "Part 2: " << getChecksum(filemap) << std::endl;
 }
