f94892f2a2d2c0f53340978aa6abdcd44ba261f1
Ben Fedidat <ben@fedidat.com>
2024-12-18 10:04:47 +0200

    Day 17 improvements

--- day_17/main.cc
+++ day_17/main.cc
@@ -35,14 +35,14 @@ class Processor {
         std::vector<uint64_t> registers, program;
         uint64_t curInstruction;
         std::vector<std::function<void(uint64_t)>> instructionSet = {
-            [this](auto operand) { registers[0] = registers[0] / std::pow(2, asCombo(operand)); }, // 0, adv, A = A // 2^(combo)
-            [this](auto operand) { registers[1] ^= operand; }, // 1, bxk, B = B XOR literal
-            [this](auto operand) { registers[1] = asCombo(operand) % 8; }, // 2, bst, B = combo % 8
-            [this](auto operand) { if (registers[0] != 0) { curInstruction = 2 * operand; } }, // 3, jnz, if A != 0, jump to literal
-            [this](auto operand) { registers[1] ^= registers[2]; }, // 4, bxc, B = B XOR C
-            [this](auto operand) { output.push_back(asCombo(operand) % 8); }, // 5, out, print combo % 8
-            [this](auto operand) { registers[1] = registers[0] / std::pow(2, asCombo(operand)); }, // 6, bdv, B = A // 2^(combo)
-            [this](auto operand) { registers[2] = registers[0] / std::pow(2, asCombo(operand)); } // 7, cdv, C = A // 2^(combo)
+            [this](auto operand) { registers[0] = registers[0] / std::pow(2, asCombo(operand)); },  // 0, adv, A = A // 2^(combo)
+            [this](auto operand) { registers[1] ^= operand; },                                      // 1, bxk, B = B XOR literal
+            [this](auto operand) { registers[1] = asCombo(operand) % 8; },                          // 2, bst, B = combo % 8
+            [this](auto operand) { if (registers[0] != 0) { curInstruction = 2 * operand; } },      // 3, jnz, if A != 0, jump to literal
+            [this](auto operand) { registers[1] ^= registers[2]; },                                 // 4, bxc, B = B XOR C
+            [this](auto operand) { output.push_back(asCombo(operand) % 8); },                       // 5, out, print combo % 8
+            [this](auto operand) { registers[1] = registers[0] / std::pow(2, asCombo(operand)); },  // 6, bdv, B = A // 2^(combo)
+            [this](auto operand) { registers[2] = registers[0] / std::pow(2, asCombo(operand)); }   // 7, cdv, C = A // 2^(combo)
         };
         uint64_t asCombo(uint64_t operand) {
             return operand < 4 ? operand : registers[operand - 4];
@@ -70,7 +70,7 @@ class Processor {
             }
             return s;
         }
-        bool sameOutput(int instructionIdx) {
+        bool outputMatchesProgram(int instructionIdx) {
             runProgram();
             for (int i=0; i<output.size() && i<program.size(); i++)
                 if (output[i] != program[instructionIdx + i])
@@ -81,9 +81,11 @@ class Processor {
 
 uint64_t findRegistersProducingOutput(std::vector<uint64_t> program, uint64_t curInstruction, uint64_t currentA)
 {
+    // A is processed in blocks of 3 bits for each output,
+    // so we recursively check which 3 bits appended to A produce the next output, from end to beginning.
     for (uint64_t i = 0; i < 8; i++) {
         uint64_t nextA = currentA * 8 + i;
-        if (Processor({nextA, 0, 0}, program).sameOutput(curInstruction)) {
+        if (Processor({nextA, 0, 0}, program).outputMatchesProgram(curInstruction)) {
             if (curInstruction == 0)
                 return nextA;
             uint64_t result = findRegistersProducingOutput(program, curInstruction - 1, nextA);
@@ -94,14 +96,10 @@ uint64_t findRegistersProducingOutput(std::vector<uint64_t> program, uint64_t cu
     return UINT64_MAX;
 }
 
-void resolve() {
+int main() 
+{
     auto [registers, program] = parseFile("input.txt");
     std::cout << "Part 1: " << Processor(registers, program).getOutput() << std::endl;
     std::cout << "Part 2: " << findRegistersProducingOutput(program, program.size()-1, 0) << std::endl;
-}
-
-int main() 
-{
-    resolve();
     return 0;
 }
